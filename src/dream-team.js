5df3425d5ad0fc55370d5a3f11254b6cec1008bd
Roman Mavrin <roma-mrv@yandex.ru>
2021-02-17 23:17:41 +0300

    add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,15 +2,17 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(x) {
   if(Array.isArray(x)){
+    let y = x.map(e=>e.split(' ').join(''))
     let newArr = []
-    for(let i = 0; i < x.length; i++){
-      if(typeof x[i] ===  'string'){
-        newArr.push(x[i])
+    for(let i = 0; i < y.length; i++){
+      if(typeof y[i] ===  'string'){
+        newArr.push(y[i])
       } 
     }
     let namePass = newArr.map(e=>e[0]).sort().join('')
-    return namePass.toUpperCase()
-  } else {
-     return false
-   }
+     
+     return namePass
+    } else {
+    return false
+  }
 };
