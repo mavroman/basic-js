b803bd7a8a0d0a0fa5c7647eb6db6a43dae6679a
Roman Mavrin <roma-mrv@yandex.ru>
2021-02-17 23:42:58 +0300

    add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -6,12 +6,12 @@ module.exports = function createDreamTeam(x) {
     let newArr = []
     for(let i = 0; i < x.length; i++){
       if(typeof x[i] ===  'string'){
-        newArr.push(x[i].split(' ').join(''))
+        newArr.push(x[i].toUpperCase().split(' ').join(''))
       } 
     }
     let namePass = newArr.map(e=>e[0]).sort().join('')
      
-     return namePass.toUpperCase()
+     return namePass
     } else {
     return false
   }
